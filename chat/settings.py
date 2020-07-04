64ad10c6e884122d4ea2b80511d5427a7eb799c3
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-07-04 18:09:53 -0300

    settings changed

--- chat/settings.py
+++ chat/settings.py
@@ -59,8 +59,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chat.wsgi.application'
-
 ASGI_APPLICATION = 'chat.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
